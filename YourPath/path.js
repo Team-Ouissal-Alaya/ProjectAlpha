7e096f7a7e4b38debdc153a2424303a3f6873f00
AlayaMohamed <alayamohamed@outlook.com>
2021-07-09 00:13:06 +0100

    first update

--- YourPath/path.js
+++ YourPath/path.js
@@ -1,80 +1,119 @@
-var mbf = ['plan1']
-var mbm = ['plan2']
-var mif = ['plan3']
-var mim = ['plan4']
-var maf = ['plan5']
-var mam = ['plan6']
-var fbf = ['plan7']
-var fbm = ['plan8']
-var fif = ['plan9']
-var fim = ['plan10']
-var faf = ['plan11']
-var fam = ['plan12']
-
 var bmale = false;
 var bfemale = false;
-var bbeginner=false;
-var bintermediate=false;
-var badvanced=false;
-var bfat=false;
-var bmuscle=false;
+var bbeginner = false;
+var bintermediate = false;
+var badvanced = false;
+var bfat = false;
+var bmuscle = false;
 
 $('#male').click(function () {
-   
+
     bmale = true
-    bfemale=false
-    $(this).css('background-color','red');
-    $('#female').css('background-color','');
-    $('#level').css('display','block')
-   
+    bfemale = false
+    $(this).css('background-color', 'lightcoral');
+    $('#female').css('background-color', '');
+    $('#level').css('display', 'block')
+    localStorage.setItem("gender", "male");
 
 })
 $('#female').click(function () {
     bfemale = true
-    bmale=false
-    $(this).css('background-color','red');
-    $('#male').css('background-color','');
-    $('#level').css('display','block')
-   
+    bmale = false
+    $(this).css('background-color', 'lightcoral');
+    $('#male').css('background-color', '');
+    $('#level').css('display', 'block')
+    localStorage.setItem("gender", "female");
+
 })
 $('#beginner').click(function () {
     bbeginner = true
-    bintermediate=false;
-    badvanced=false;
-    $(this).css('background-color','red');
-    $('#intermediate').css('background-color','');
-    $('#advanced').css('background-color','');
-    $('#exercicegoal').css('display','block')
+    bintermediate = false;
+    badvanced = false;
+    $(this).css('background-color', 'lightcoral');
+    $('#intermediate').css('background-color', '');
+    $('#advanced').css('background-color', '');
+    $('#exercicegoal').css('display', 'block')
+    localStorage.setItem("experience", "beginner");
 
 })
 $('#intermediate').click(function () {
     bintermediate = true;
-    bbeginner=false;
-    badvanced=false
-    $(this).css('background-color','red');
-    $('#beginner').css('background-color','');
-    $('#advanced').css('background-color','');
-    $('#exercicegoal').css('display','block')
+    bbeginner = false;
+    badvanced = false
+    $(this).css('background-color', 'lightcoral');
+    $('#beginner').css('background-color', '');
+    $('#advanced').css('background-color', '');
+    $('#exercicegoal').css('display', 'block')
+    localStorage.setItem("experience", "intermediate");
 })
 $('#advanced').click(function () {
     badvanced = true;
-    bbeginner=false;
-    bintermediate=false;
-    $(this).css('background-color','red');
-    $('#beginner').css('background-color','');
-    $('#intermediate').css('background-color','');
-    $('#exercicegoal').css('display','block')
+    bbeginner = false;
+    bintermediate = false;
+    $(this).css('background-color', 'lightcoral');
+    $('#beginner').css('background-color', '');
+    $('#intermediate').css('background-color', '');
+    $('#exercicegoal').css('display', 'block')
+    localStorage.setItem("experience", "advanced");
 })
 $('#fat').click(function () {
     bfat = true;
-    bmuscle=false;
-    $(this).css('background-color','red');
-    $('#muscle').css('background-color','');
+    bmuscle = false;
+    $(this).css('background-color', 'lightcoral');
+    $('#muscle').css('background-color', '');
+    localStorage.setItem("goal", "fat");
+    
 })
 $('#muscle').click(function () {
     bmuscle = true;
-    bfat=false;
-    $(this).css('background-color','red');
-    $('#fat').css('background-color','');
-    
+    bfat = false;
+    $(this).css('background-color', 'lightcoral');
+    $('#fat').css('background-color', '');
+    localStorage.setItem("goal", "muscle");
+
 })
+
+function workplan() {
+    if (bmale === true && bbeginner === true && bfat === true) {
+        window.location.href = ""
+    }
+
+    if (bmale === true && bbeginner === true && bmuscle === true) {
+        window.location.href = ""
+    }
+
+    if (bmale === true && bintermediate === true && bfat === true) {
+        window.location.href = ""
+    }
+    if (bmale === true && bintermediate === true && bmuscle === true) {
+        window.location.href = ""
+    }
+    if (bmale === true && badvanced === true && bfat === true) {
+        window.location.href = ""
+    }
+    if (bmale === true && badvanced === true && bmuscle === true) {
+        window.location.href = ""
+    }
+
+    if (bfemale === true && bbeginner === true && bfat === true) {
+        window.location.href = ""
+    }
+
+    if (bfemale === true && bbeginner === true && bmuscle === true) {
+        window.location.href = ""
+    }
+
+    if (bfemale === true && bintermediate === true && bfat === true) {
+        window.location.href = ""
+    }
+    if (bfemale === true && bintermediate === true && bmuscle === true) {
+        window.location.href = ""
+    }
+    if (bfemale === true && badvanced === true && bfat === true) {
+        window.location.href = ""
+    }
+    if (bfemale === true && badvanced === true && bmuscle === true) {
+        window.location.href = ""
+    }
+
+}
---
+++ workplans/path1.js
@@ -0,0 +1,10 @@
+
+$('#start').click(function(){
+    $('#inputs').css('display', 'block')
+})
+
+
+$('#day1').click(function(){
+    $('#photy').append($('#phot1'))
+    $('#photy').css('display', 'block')
+})
